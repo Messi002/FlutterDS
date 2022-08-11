b0d679943c540bd0a54aaedbb9bb103c223c1c57
Messi002 <rostandmessi2@gmail.com>
2022-08-11 12:04:07 +0100

    FlutterUIs

--- BigProjects/flut_app2/lib/Pages/welcome_page.dart
+++ BigProjects/flut_app2/lib/Pages/welcome_page.dart
@@ -1,5 +1,6 @@
-// ignore_for_file: prefer_const_literals_to_create_immutables
+// ignore_for_file: prefer_const_literals_to_create_immutables, sized_box_for_whitespace, prefer_const_constructors
 
+import 'package:flut_app/constants.dart';
 import 'package:flut_app/utils/app_large_text.dart';
 import 'package:flut_app/utils/app_text.dart';
 import 'package:flutter/material.dart';
@@ -22,6 +23,8 @@ class _WelcomeState extends State<Welcome> {
       "welcome-two.png",
       "welcome-three.png"
     ];
+
+  
 
     return Scaffold(
       body: PageView.builder(
@@ -48,11 +51,15 @@ class _WelcomeState extends State<Welcome> {
                           text: "Mountain",
                           size: 30,
                         ),
+                        SizedBox(height: 20,),
                         Container(
                           width: 250,
                           child: AppText(
-                              text:
-                                  "Mountain hikes give you an incredible sense of freedom"),
+                            text:
+                                "Mountain hikes give you an incredible sense of freedom",
+                            color: AppColors.textColor2,
+                            size: 14,
+                          ),
                         ),
                       ],
                     )
