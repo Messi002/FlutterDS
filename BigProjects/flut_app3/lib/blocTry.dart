2722331f4c7da796395752104b39841bc7cb1e27
Messi002 <rostandmessi2@gmail.com>
2022-08-11 09:57:03 +0100

    FlutterUIs

--- BigProjects/flut_app3/lib/blocTry.dart
+++ BigProjects/flut_app3/lib/blocTry.dart
@@ -2,20 +2,22 @@
 // import 'package:flutter_bloc/flutter_bloc.dart';
 // import 'package:bloc/bloc.dart';
 
-void main(List<String> args) {
-  Stream<int> countStream(int max) async* {
-    for (int i = 0; i < max; i++) {
-      yield i;
-      print(i);
-    }
-  }
+// Stream<int> countStream(int max) async* {
+//   for (int i = 0; i < max; i++) {
+//     yield i;
+//   }
+// }
 
-  Future<int> sumStream(Stream<int> stream) async {
-    int sum = 0;
-    await for (int value in stream) {
-      sum += value;
-    }
-    return sum;
-  }
+// Future<int> sumStream(Stream<int> stream) async {
+//   int sum = 0;
+//   await for (int value in stream) {
+//     sum += value;
+//   }
+//   return sum;
+// }
 
-}
+// void main(List<String> args) async {
+//   Stream<int> stream = countStream(8);
+//   int sum = await sumStream(stream);
+//   print(sum);
+// }
