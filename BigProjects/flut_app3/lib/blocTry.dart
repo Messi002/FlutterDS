0dcb0b78997cc798f93a8728fbe595d995adfa17
Messi002 <rostandmessi2@gmail.com>
2022-08-11 10:17:46 +0100

    FlutterUIs

--- BigProjects/flut_app3/lib/blocTry.dart
+++ BigProjects/flut_app3/lib/blocTry.dart
@@ -1,6 +1,6 @@
 // import 'package:flutter/material.dart';
 // import 'package:flutter_bloc/flutter_bloc.dart';
-// import 'package:bloc/bloc.dart';
+import 'package:bloc/bloc.dart';
 
 // Stream<int> countStream(int max) async* {
 //   for (int i = 0; i < max; i++) {
@@ -20,4 +20,35 @@
 //   Stream<int> stream = countStream(8);
 //   int sum = await sumStream(stream);
 //   print(sum);
+// }
+
+class CounterCubit extends Cubit<int> {
+  CounterCubit() : super(0);
+
+  void increment() => emit(state + 1);
+
+  @override
+  void onChange(Change<int> change) {
+    super.onChange(change);
+    print(change);
+  }
+}
+
+Future<void> main(List<String> args) async {
+  final cubit = CounterCubit();
+  final subscription = cubit.stream.listen(print);
+  cubit.increment();
+  await Future.delayed(Duration.zero);
+  await subscription.cancel();
+  await cubit.close();
+}
+
+// void main(List<String> args) {
+//   final cubit = CounterCubit();
+//   print(cubit.state);
+//   cubit.increment();
+//   print(cubit.state);
+//     cubit.increment();
+//   print(cubit.state);
+//   cubit.close();
 // }
