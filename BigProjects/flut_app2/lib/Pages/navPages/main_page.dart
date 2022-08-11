2562888f24fdd5a24c2c27d51c7cb539307dd42f
Messi002 <rostandmessi2@gmail.com>
2022-08-11 13:59:20 +0100

    FlutterUIs

--- BigProjects/flut_app2/lib/Pages/navPages/main_page.dart
+++ BigProjects/flut_app2/lib/Pages/navPages/main_page.dart
@@ -1,5 +1,9 @@
 // ignore_for_file: prefer_const_constructors, avoid_unnecessary_containers, prefer_const_literals_to_create_immutables
 
+import 'package:flut_app/Pages/navPages/bar_item_page.dart';
+import 'package:flut_app/Pages/navPages/home_page.dart';
+import 'package:flut_app/Pages/navPages/profile_page.dart';
+import 'package:flut_app/Pages/navPages/search_page.dart';
 import 'package:flutter/material.dart';
 
 class MainPage extends StatefulWidget {
@@ -10,17 +14,24 @@ class MainPage extends StatefulWidget {
 }
 
 class _MainPageState extends State<MainPage> {
+  List pages = [
+    HomePage(),
+    BarItem(),
+    SearchPage(),
+    MyPage(),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       bottomNavigationBar: BottomNavigationBar(
         items: [
-          BottomNavigationBarItem(label:"Home", icon:Icon(Icons.apps)),
-          BottomNavigationBarItem(label:"Bar", icon:Icon(Icons.bar_chart_sharp,)),
-          BottomNavigationBarItem(label:"Search", icon:Icon(Icons.search)),
-          BottomNavigationBarItem(label:"My", icon:Icon(Icons.person)),
+          BottomNavigationBarItem(label: "Home", icon: Icon(Icons.apps)),
+          BottomNavigationBarItem(label: "Bar",icon: Icon(Icons.bar_chart_sharp)),
+          BottomNavigationBarItem(label: "Search", icon: Icon(Icons.search)),
+          BottomNavigationBarItem(label: "My", icon: Icon(Icons.person)),
         ],
-        ),
+      ),
     );
   }
 }
