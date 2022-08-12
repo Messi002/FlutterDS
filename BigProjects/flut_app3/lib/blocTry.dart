9c0e00aca1b6cb2c4456530e1027387d729d934c
Messi002 <rostandmessi2@gmail.com>
2022-08-12 11:00:05 +0100

    FlutterUIs

--- BigProjects/flut_app3/lib/blocTry.dart
+++ BigProjects/flut_app3/lib/blocTry.dart
@@ -61,11 +61,13 @@ import 'package:bloc/bloc.dart';
 class CounterCubit extends Cubit<int> {
   CounterCubit() : super(0);
 
-  void increment() => emit(state + 1);
+  void increment() {
+    addError(Exception('increment error!'), StackTrace.current);
+    emit(state + 1);
+  }
 
   @override
   void onChange(Change<int> change) {
-
     super.onChange(change);
     print(change);
   }
