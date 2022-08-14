fac04d6aa7577229c066faea79bc2a05337d0d14
Messi002 <rostandmessi2@gmail.com>
2022-08-14 02:57:27 +0100

    FlutterUIs

--- BigProjects/flut_app3/lib/bloc/bloc_actions.dart
+++ BigProjects/flut_app3/lib/bloc/bloc_actions.dart
@@ -1,10 +1,12 @@
+import 'package:flut_app/bloc/person.dart';
 import 'package:flutter/foundation.dart' show immutable;
 
-const Person1Url = "http://127.0.0.1:5500/BigProjects/flut_app3/api/persons1.json";
-const Person2Url = "http://127.0.0.1:5500/BigProjects/flut_app3/api/persons2.json";
+const person1Url =
+    "http://127.0.0.1:5500/BigProjects/flut_app3/api/persons1.json";
+const person2Url =
+    "http://127.0.0.1:5500/BigProjects/flut_app3/api/persons2.json";
 
-
-
+typedef PersonsLoader = Future<Iterable<Person>> Function(String url);
 
 @immutable
 abstract class LoadAction {
@@ -14,7 +16,6 @@ abstract class LoadAction {
 @immutable
 class LoadPersonAction implements LoadAction {
   final String url;
-
-  const LoadPersonAction({required this.url}) : super();
+  final PersonsLoader loader;
+  const LoadPersonAction({required this.url, required this.loader}) : super();
 }
-
