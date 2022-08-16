110035309ffeffd2a8f33e0d3c945b3b694a5f69
Messi002 <rostandmessi2@gmail.com>
2022-08-16 11:23:24 +0100

    FlutterUIs

--- BigProjects/flut_app 4/lib/counter_page.dart
+++ BigProjects/flut_app 4/lib/counter_page.dart
@@ -1,8 +1,8 @@
+import 'package:flut_app/counter_bloc.dart';
 import 'package:flutter/material.dart';
 import 'package:bloc/bloc.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-
-
+import 'counter_bloc.dart';
 
 class CounterPage extends StatelessWidget {
   const CounterPage({Key? key}) : super(key: key);
@@ -10,8 +10,14 @@ class CounterPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("Counter"),),
-      body: ,
+      appBar: AppBar(
+        title: const Text("Counter"),
+      ),
+      body: BlocBuilder<CounterBloc, int>(builder: (context, state) {
+        return Center(
+          child:Text("$state",style: TextStyle(fontSize: 24.0),)
+        );
+      }),
     );
   }
 }
