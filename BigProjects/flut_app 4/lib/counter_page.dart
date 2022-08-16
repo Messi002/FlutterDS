1746c4410ebcb13087199e59efb33e99073809b5
Messi002 <rostandmessi2@gmail.com>
2022-08-16 11:17:52 +0100

    FlutterUIs

---
+++ BigProjects/flut_app 4/lib/counter_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:bloc/bloc.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+
+
+class CounterPage extends StatelessWidget {
+  const CounterPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Counter"),),
+      body: ,
+    );
+  }
+}
