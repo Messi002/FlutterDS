50d1380e9db249e17702082372c5b071aac5da14
Messi002 <rostandmessi2@gmail.com>
2022-08-16 12:40:19 +0100

    FlutterUIs

---
+++ BigProjects/flut_app 4/lib/counter/view/counter_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flut_app/counter/cubit/counter_cubit.dart';
+import 'package:flut_app/counter/view/counter_view.dart';
+
+class CounterPage extends StatelessWidget {
+  const CounterPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    BlocProvider(create: (_) => CounterCubit(),
+    child: CounterView(),
+    );
+  }
+}
---
+++ BigProjects/flut_app 4/lib/counter/view/counter_view.dart
@@ -0,0 +1,22 @@
+import 'package:flut_app/counter/cubit/counter_cubit.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class CounterView extends StatelessWidget {
+  const CounterView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final textTheme = Theme.of(context).textTheme;
+    return Scaffold(
+      appBar: AppBar(title: const Text('Counter')),
+      body: Center(
+        child: BlocBuilder<CounterCubit, int>(
+          builder: (context, state) {
+            return Text('$state', style: textTheme.headline2,);
+          },
+        ),
+      ),
+    );
+  }
+}
