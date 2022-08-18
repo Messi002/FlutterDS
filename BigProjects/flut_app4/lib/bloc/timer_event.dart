f51b6647a74dd5862e499840f9928344cd6314b5
Messi002 <rostandmessi2@gmail.com>
2022-08-18 10:50:13 +0100

    FlutterUIs

--- BigProjects/flut_app4/lib/bloc/timer_event.dart
+++ BigProjects/flut_app4/lib/bloc/timer_event.dart
@@ -11,3 +11,23 @@ class TimerStarted extends TimerEvent {
   const TimerStarted({required this.duration});
   final int duration;
 }
+
+class TimerPaused extends TimerEvent {
+  const TimerPaused();
+}
+
+class TimerResumed extends TimerEvent {
+  const TimerResumed();
+}
+
+class TimerReset extends TimerEvent {
+  const TimerReset();
+}
+
+class TimerTicked extends TimerEvent {
+  const TimerTicked({required this.duration});
+  final int duration;
+
+  @override
+  List<Object?> get props => [duration];
+}
