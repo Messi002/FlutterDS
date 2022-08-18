f224a8ebb61ff893eceb3873cca0c606b24704f8
Messi002 <rostandmessi2@gmail.com>
2022-08-18 12:20:10 +0100

    FlutterUIs

--- BigProjects/flut_app4/lib/timer/view/timer_page.dart
+++ BigProjects/flut_app4/lib/timer/view/timer_page.dart
@@ -52,12 +52,44 @@ class TimerText extends StatelessWidget {
     final duration = context.select((TimerBloc bloc) => bloc.state.duration);
     final minutesStr =
         ((duration / 60) % 60).floor().toString().padLeft(2, '0');
-    final secondStr = 
-        (duration / 60).floor().toString().padLeft(2, '0');
+    final secondStr = (duration / 60).floor().toString().padLeft(2, '0');
 
     return Text(
       '$minutesStr : $secondStr',
       style: Theme.of(context).textTheme.headline1,
+    );
+  }
+}
+
+class Action extends StatelessWidget {
+  const Action({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<TimerBloc, TimerState>(
+      buildWhen: (previous, current) =>  prev.runtimeType != state.runtimeType,
+      builder: ((context, state) {
+        return Row();
+      }
+      );
+  }
+}
+
+class Background extends StatelessWidget {
+  const Background({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [
+              Colors.blue.shade50,
+              Colors.blue.shade500,
+            ]),
+      ),
     );
   }
 }
