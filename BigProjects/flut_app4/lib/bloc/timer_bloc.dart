d87e44dbddde0c503efe73ebcc40b63a661146f8
Messi002 <rostandmessi2@gmail.com>
2022-08-18 11:36:44 +0100

    FlutterUIs

--- BigProjects/flut_app4/lib/bloc/timer_bloc.dart
+++ BigProjects/flut_app4/lib/bloc/timer_bloc.dart
@@ -10,5 +10,24 @@ class TimerBloc extends Bloc<TimerEvent, TimerState> {
 
   StreamSubscription<int>? _tickerSubscription;
 
-  TimerBloc({required Ticker ticker}) : _ticker = ticker, super(const TimerInitial(_duration));
+  TimerBloc({required Ticker ticker})
+      : _ticker = ticker,
+        super(const TimerInitial(_duration)) {
+    on<TimerStarted>(_onStarted);
+  }
+
+  @override
+  Future<void> close() {
+    //TODO: change the null checker back to a ?
+    _tickerSubscription!.cancel();
+    return super.close();
+  }
+
+  void _onStarted(TimerStarted event, Emitter<TimerState> emit) {
+    emit(TimerRunInProgress(event.duration));
+    _tickerSubscription!.cancel();
+    _tickerSubscription = _ticker
+        .tick(ticks: event.duration)
+        .listen((duration) => add(TimerTicked(duration: duration)));
+  }
 }
