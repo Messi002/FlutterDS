a5ac432d9ed148015e79332ea1c2250869cbc194
Messi002 <rostandmessi2@gmail.com>
2022-08-18 11:56:23 +0100

    FlutterUIs

--- BigProjects/flut_app4/lib/main.dart
+++ BigProjects/flut_app4/lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 
-void main() => runApp(MyApp());
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
@@ -10,7 +10,13 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Hello',
-      
+      theme: ThemeData(
+        primaryColor: const Color.fromRGBO(109, 234, 255, 1),
+        colorScheme: const ColorScheme.light(
+          secondary: Color.fromRGBO(72, 74, 126, 1),
+        )
+      ),
+      home: const TimerPage(),
     );
   }
 }
