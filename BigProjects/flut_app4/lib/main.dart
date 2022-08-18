127e3d69d3c1b09a831338640392ad8cf391f5b6
Messi002 <rostandmessi2@gmail.com>
2022-08-18 12:33:26 +0100

    FlutterUIs

--- BigProjects/flut_app4/lib/main.dart
+++ BigProjects/flut_app4/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flut_app/timer/view/timer_page.dart';
 
 
 void main() => runApp(const MyApp());
--- BigProjects/flut_app4/lib/timer/view/timer_page.dart
+++ BigProjects/flut_app4/lib/timer/view/timer_page.dart
@@ -5,8 +5,8 @@ import 'package:flut_app/ticker.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-class TimperPage extends StatelessWidget {
-  const TimperPage({Key? key}) : super(key: key);
+class TimerPage extends StatelessWidget {
+  const TimerPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
