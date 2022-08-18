81ff53ca2c16fbcd06cf65917aeee31fe0145211
Messi002 <rostandmessi2@gmail.com>
2022-08-18 12:12:59 +0100

    FlutterUIs

--- BigProjects/flut_app4/lib/timer/view/timer_page.dart
+++ BigProjects/flut_app4/lib/timer/view/timer_page.dart
@@ -29,10 +29,12 @@ class TimerView extends StatelessWidget {
             mainAxisAlignment: MainAxisAlignment.center,
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              Padding(padding: EdgeInsets.symmetric(vertical: 100.0),
-              child: Center(
-                child: TimerText(),
-              ),),
+              Padding(
+                padding: EdgeInsets.symmetric(vertical: 100.0),
+                child: Center(
+                  child: TimerText(),
+                ),
+              ),
               Action()
             ],
           )
@@ -42,4 +44,20 @@ class TimerView extends StatelessWidget {
   }
 }
 
+class TimerText extends StatelessWidget {
+  const TimerText({Key? key}) : super(key: key);
 
+  @override
+  Widget build(BuildContext context) {
+    final duration = context.select((TimerBloc bloc) => bloc.state.duration);
+    final minutesStr =
+        ((duration / 60) % 60).floor().toString().padLeft(2, '0');
+    final secondStr = 
+        (duration / 60).floor().toString().padLeft(2, '0');
+
+    return Text(
+      '$minutesStr : $secondStr',
+      style: Theme.of(context).textTheme.headline1,
+    );
+  }
+}
