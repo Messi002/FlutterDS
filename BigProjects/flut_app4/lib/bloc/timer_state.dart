66706f598ef270825da59e7d2a2b04c5918a2021
Messi002 <rostandmessi2@gmail.com>
2022-08-18 10:40:37 +0100

    FlutterUIs

--- BigProjects/flut_app4/lib/bloc/timer_state.dart
+++ BigProjects/flut_app4/lib/bloc/timer_state.dart
@@ -1,8 +1,29 @@
-part of timer_bloc.dart;
-
-// import 'package:equatable/equatable.dart';
+import 'package:equatable/equatable.dart';
+// part of timer_bloc.dart;
 
 abstract class TimerState extends Equatable {
   const TimerState(this.duration);
   final int duration;
+
+  @override
+  List<Object?> get props => [duration];
 }
+
+class TimerInitial extends TimerState {
+  TimerInitial(super.duration);
+
+  @override
+  String toString() {
+    return 'TimerInitial {duration: $duration}';
+  }
+}
+
+class TimerRunPause extends TimerState{
+  TimerRunPause(super.duration);
+  @override
+  String toString() {
+    return 'TimerRunPause {duration: $duration}';
+  }
+}
+
+class
