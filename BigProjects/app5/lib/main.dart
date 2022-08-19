0f2d47aede61359fd9c88e2863a1d57bd3615b75
Messi002 <rostandmessi2@gmail.com>
2022-08-19 17:05:29 +0100

    FlutterUIs

--- BigProjects/app5/lib/main.dart
+++ BigProjects/app5/lib/main.dart
@@ -2,6 +2,7 @@ import 'package:app5/store_bindings.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
+import 'Views/store_status.dart';
 import 'home.dart';
 
 void main() => runApp(const MyApp());
@@ -12,9 +13,22 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
+      initialRoute: '/',
       initialBinding: StoreBinding(),
       debugShowCheckedModeBanner: false,
       title: 'Demo',
+       getPages: [
+        GetPage(
+          name: '/',
+          page: () => HomePage(),
+        ),
+        // GetPage(name: '/edit_name', page: () => UpdateStoreName()),
+        // GetPage(name: '/add_followers', page: () => AddFollowers()),
+        GetPage(name: '/toggle_status', page: () => StoreStatus()),
+        // GetPage(name: '/edit_follower_count', page: () => AddFollowerCount()),
+        // GetPage(name: '/add_reviews', page: () => AddReviews()),
+        // GetPage(name: '/update_menu', page: () => const UpdateMenu()),
+      ],
       home:const HomePage(),
     );
   }
