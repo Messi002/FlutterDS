fa3a7f6e3aa46001489460c384768f8c36224463
Messi002 <rostandmessi2@gmail.com>
2022-08-19 13:56:51 +0100

    FlutterUIs

--- BigProjects/app5/lib/Controllers/StoreController.dart
+++ BigProjects/app5/lib/Controllers/StoreController.dart
@@ -1,4 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-class StoreController extends
+class StoreController extends GetxController {
+  final storeName = 'MIRShop'.obs;
+  final followerCount = 0.obs;
+  final storeStatus = true.obs;
+  final followerList = [].obs;
+  final reviews = [].obs;
+
+  final storeNameEditingController = TextEditingController();
+  final followerController = TextEditingController();
+  final reeviewNameEditingController = TextEditingController();
+  final reviewEditingController = TextEditingController();
+
+  updateStoreName(String name) {
+    storeName(name);
+  }
+
+  updatefollowerCount() {
+    followerCount(followerCount.value++);
+  }
+    updateStoreStatus(String name) {
+    storeName(name);
+  }  updateStoreName(String name) {
+    storeName(name);
+  }
+}
