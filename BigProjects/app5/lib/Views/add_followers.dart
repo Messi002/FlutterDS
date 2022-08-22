5f0666a061c5b50a7998273b2f8eaff7bd376001
Messi002 <rostandmessi2@gmail.com>
2022-08-22 08:08:29 +0100

    FlutterUIs

--- BigProjects/app5/lib/Views/add_followers.dart
+++ BigProjects/app5/lib/Views/add_followers.dart
@@ -1,14 +1,34 @@
+// ignore_for_file: unnecessary_string_escapes
+
 import 'package:app5/Controllers/store_controller.dart';
+import 'package:app5/Utils/input.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class AddFollowers extends StatelessWidget {
-   AddFollowers({Key? key}) : super(key: key);
+  AddFollowers({Key? key}) : super(key: key);
   final storeController = Get.find<StoreController>();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: App,
+      appBar: AppBar(
+        title: const Text('Add Followers'),
+      ),
+      body: Column(
+        children: <Widget>[
+          Input(
+            hintText: 'Enter Follower\s name...',
+            controller: storeController.followerController,
+          ),
+          ElevatedButton.icon(
+              onPressed: () {
+                storeController.addNewFollower(
+                    storeController.followerController.text.trim());
+              },
+              icon: Icon(Icons.person_add_alt_rounded),
+              label: Text("Follower"))
+        ],
+      ),
     );
   }
 }
