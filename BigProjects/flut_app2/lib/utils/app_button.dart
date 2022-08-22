6077dfddc490db7e06aefd1410adab84b96bf044
Messi002 <rostandmessi2@gmail.com>
2022-08-22 12:36:03 +0100

    FlutterUIs

--- BigProjects/flut_app2/lib/utils/app_button.dart
+++ BigProjects/flut_app2/lib/utils/app_button.dart
@@ -1,11 +1,27 @@
+import 'package:flut_app/constants.dart';
 import 'package:flutter/material.dart';
 
-
 class AppButton extends StatelessWidget {
-  const AppButton({Key? key}) : super(key: key);
+  AppButton({
+    super.key,
+    required this.widget, required this.color, required this.bordercolor, required this.bgcolor,
+  });
+
+  final Widget widget;
+  final Color color;
+  final Color bordercolor;
+  final Color bgcolor;
+  double? size;
 
   @override
   Widget build(BuildContext context) {
-    
+    return Container(
+      width: 60,
+      height: 60,
+      decoration: BoxDecoration(
+          color: AppColors.buttonBackground,
+          borderRadius: BorderRadius.circular(15)),
+      child: widget,
+    );
   }
 }
