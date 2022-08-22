94b453b9d255a19f3755adc9586536486ca157b3
Messi002 <rostandmessi2@gmail.com>
2022-08-22 11:31:43 +0100

    FlutterUIs

--- BigProjects/flut_app2/lib/Pages/detail_page.dart
+++ BigProjects/flut_app2/lib/Pages/detail_page.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_literals_to_create_immutables
+
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/material.dart';
@@ -17,7 +19,39 @@ class _DetailPageState extends State<DetailPage> {
         width: double.maxFinite,
         height: double.maxFinite,
         child: Stack(
-          
+          children: [
+            Positioned(
+                left: 0,
+                right: 0,
+                child: Container(
+                  height: 350,
+                  width: double.maxFinite,
+                  decoration: const BoxDecoration(
+                      image: DecorationImage(
+                          image: AssetImage('img/mountain.jpeg'),
+                          fit: BoxFit.cover)),
+                )),
+            Positioned(
+                left: 20,
+                height: 70,
+                child: Row(
+                  children: [
+                    const IconButton(
+                        onPressed: null,
+                        icon: Icon(
+                          Icons.menu,
+                          color: Color.fromARGB(255, 230, 224, 224),
+                        ))
+                  ],
+                )),
+
+            Positioned(child: Container(
+              decoration: BoxDecoration(
+                color: Colors.red,
+                
+              ),
+            )),
+          ],
         ),
       ),
     );
