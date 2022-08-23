226ae6601dd05055354731cf2777db7262e0dac0
Messi002 <rostandmessi2@gmail.com>
2022-08-24 00:46:19 +0100

    FlutterUIs

---
+++ BigProjects/flutt_app2/lib/cubit/app_cubits.dart
@@ -0,0 +1,9 @@
+
+
+import 'package:flut_app/cubit/app_cubit_states.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class AppCubit extends Cubit{
+  AppCubit():super(InitialState());
+
+}
