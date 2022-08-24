ba4156bf2be7ed2d3e9167c257b38e88f1a1ee7a
Messi002 <rostandmessi2@gmail.com>
2022-08-24 01:22:56 +0100

    FlutterUIs

--- BigProjects/flutt_app2/lib/services/data_services.dart
+++ BigProjects/flutt_app2/lib/services/data_services.dart
@@ -1,12 +1,24 @@
 import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 
 class DataServices {
   String baseUrl = "http://mark.bslmeiyu.com/api";
 
-  getInfo(){
+  getInfo() async{
     var apiUrl = '/getplaces';
 
-    http.get()
+ return  http.Response res = await http.get(Uri.parse(baseUrl + apiUrl));
+
+ try{
+  if(res.statusCode == 200){
+
+    List<dynamic> list = json.decode(res.body);
+  }
+
+
+ }catch(e){
+  print(e)
+ }
   }
 }
