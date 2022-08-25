4286db121218a8f3ddd517ff6cf4ff14685def32
Messi002 <rostandmessi2@gmail.com>
2022-08-25 19:15:09 +0100

    FlutterUIs

--- BigProjects/getx1/lib/utils/themes.dart
+++ BigProjects/getx1/lib/utils/themes.dart
@@ -1,1 +1,21 @@
 import 'package:flutter/material.dart';
+
+class Themes {
+  static final lightTheme = ThemeData(
+    scaffoldBackgroundColor: Colors.white,
+    textTheme:const  TextTheme(
+      headline1: TextStyle(
+        color: Colors.black,
+        fontSize: 26,
+      ),
+      headline2: TextStyle(
+        color: Colors.black,
+        fontSize: 15,
+      ),
+    ),
+    iconTheme: const IconThemeData(color : Colors.black, size:20 )
+  );
+
+
+  //
+}
