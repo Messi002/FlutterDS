c16e3ab3f409f0d0fc569bb7427d92a3bae933a5
Messi002 <rostandmessi2@gmail.com>
2022-08-25 19:10:57 +0100

    FlutterUIs

--- BigProjects/getx1/lib/utils/buttons.dart
+++ BigProjects/getx1/lib/utils/buttons.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:app5/Controller/theme_controller.dart';
@@ -10,7 +12,19 @@ class ButtonWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: onTap,
-      child:,
+      child: GetBuilder(
+          id: 1,
+          builder: (ctx) {
+            return Container(
+              width: Get.width /5,
+              height: Get.height / 14,
+              decoration: BoxDecoration(
+                color: _themeController.isDarkMode ? const Color.fromARGB(255, 21, 21, 21) : Colors.white,
+                borderRadius: BorderRadius.circular(20)
+              ),
+              child: Text(_themeController.isDarkMode? 'Switch to Light Mode' : 'Switch to Dark Mode', style: TextStyle(color: Colors.blue,fontSize: 19)),
+            );
+          }),
     );
   }
 }
