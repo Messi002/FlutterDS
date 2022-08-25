78156e29e1244adbb87f2866d7f3f90158325762
Messi002 <rostandmessi2@gmail.com>
2022-08-25 22:37:59 +0100

    FlutterUIs

--- BigProjects/getx1/Timer_GetX/main.dart
+++ BigProjects/getx1/Timer_GetX/main.dart
@@ -1,18 +1,24 @@
 // ignore_for_file: prefer_const_constructors
 
-
+import 'package:app5/Controller/theme_controller.dart';
+import 'package:app5/utils/themes.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'home_page.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   MyApp({Key? key}) : super(key: key);
+  final themeController = Get.put(ThemeController());
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
       debugShowCheckedModeBanner: false,
-         title: 'Demo',
+      theme: MyThemes.lightTheme,
+      darkTheme: MyThemes.darkTheme,
+      themeMode: themeController.theme,
+      title: 'Demo',
       home: HomePage(),
     );
   }
