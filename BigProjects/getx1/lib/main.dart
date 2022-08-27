c5934d2a4310189bf85045a4edfa0bf953a5d682
Messi002 <rostandmessi2@gmail.com>
2022-08-27 19:27:02 +0100

    FlutterUIs

--- BigProjects/getx1/lib/main.dart
+++ BigProjects/getx1/lib/main.dart
@@ -8,14 +8,7 @@ void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   MyApp({Key? key}) : super(key: key);
-  @override
-  Widget build(BuildContext context) {
-    return GetMaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData().copyWith(
-        textTheme: TextTheme(
-          bodyText1: TextStyle(
-            fontSize: 16,
+  @o
             color: Colors.black,
           ),
           bodyText2: TextStyle(
