7a4831cdf0f05a464bc71b31ff2fdcc8ee7f4616
Messi002 <rostandmessi2@gmail.com>
2022-08-27 19:26:16 +0100

    FlutterUIs

--- BigProjects/app5/lib/Views/update_store_name.dart
+++ BigProjects/app5/lib/Views/update_store_name.dart
@@ -1,4 +1,12 @@
-ind<StoreController>();
+import 'package:app5/Utils/input.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import '../Controllers/store_controller.dart';
+
+class UpdateStoreName extends StatelessWidget {
+  UpdateStoreName({Key? key}) : super(key: key);
+  final storeController = Get.find<StoreController>();
 
   @override
   Widget build(BuildContext context) {
