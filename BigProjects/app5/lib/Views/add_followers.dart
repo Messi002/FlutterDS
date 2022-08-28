14d895f15edf9f232a39c24ae9e6ab27f9a66a76
Messi002 <rostandmessi2@gmail.com>
2022-08-28 21:28:55 +0100

    FlutterUIs

--- BigProjects/app5/lib/Views/add_followers.dart
+++ BigProjects/app5/lib/Views/add_followers.dart
@@ -41,7 +41,13 @@ class AddFollowers extends StatelessWidget {
                 icon: const Icon(Icons.person_add_alt_rounded),
                 label: const Text("Follower")),
 
-          ,
+                 Obx(
+              () => ListView.builder(
+                shrinkWrap: true,
+                itemCount: storeController.followerList.length,
+                itemBuilder: (context, index) {
+                  return ListTile(
+                    title: Text(storeController.followerList[index]),
                   );
                 },
               ),
