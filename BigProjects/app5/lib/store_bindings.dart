0f9ba66557eb748973167b2c692c1c700e18db2a
Messi002 <rostandmessi2@gmail.com>
2022-08-28 21:29:08 +0100

    FlutterUIs

--- BigProjects/app5/lib/store_bindings.dart
+++ BigProjects/app5/lib/store_bindings.dart
@@ -1,9 +1,3 @@
 import 'package:app5/Controllers/store_controller.dart';
 import 'package:get/get.dart';
 
-class StoreBinding extends Bindings {
-  @override
-  void dependencies() {
-    Get.lazyPut(() => StoreController());
-  }
-}
