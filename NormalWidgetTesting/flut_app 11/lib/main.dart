72d494e9df56db80c614a39ca9918e2e7da61bfb
Messi002 <rostandmessi2@gmail.com>
2022-08-29 21:26:46 +0100

    FlutterUIs

--- NormalWidgetTesting/flut_app 11/lib/main.dart
+++ NormalWidgetTesting/flut_app 11/lib/main.dart
@@ -13,10 +13,9 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-      
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(),
+      home: LogoApp(),
     );
   }
 }
@@ -28,9 +27,52 @@ class LogoApp extends StatefulWidget {
   State<LogoApp> createState() => _LogoAppState();
 }
 
-class _LogoAppState extends State<LogoApp> {
+class _LogoAppState extends State<LogoApp> with SingleTickerProviderStateMixin {
+  late Animation<double> animation;
+  late AnimationController controller;
+
+  @override
+  void initState() {
+    super.initState();
+    controller =
+        AnimationController(duration: const Duration(seconds: 3), vsync: this);
+    animation = CurvedAnimation(parent: controller, curve: Curves.easeIn)
+      ..addStatusListener((status) {
+        if (status == AnimationStatus.completed) {
+          controller.reverse();
+        } else if (status == AnimationStatus.dismissed) {
+          controller.forward();
+        }
+      });
+    controller.forward();
+  }
+
+  @override
+  Widget build(BuildContext context) => AnimatedLogo(animation: animation);
+
+  @override
+  void dispose() {
+    super.dispose();
+    controller.dispose();
+  }
+}
+
+class AnimatedLogo extends AnimatedWidget {
+  AnimatedLogo({super.key, required Animation<double> animation})
+      : super(listenable: animation);
+
+  static final _opacityTween = Tween<double>(begin: 0.1, end: 1);
+  static final _sizeTween = Tween<double>(begin: 0, end: 300);
+
   @override
   Widget build(BuildContext context) {
-    
+    final animation = listenable as Animation<double>;
+    return Center(
+      child: Opacity(opacity: _opacityTween.evaluate(animation),
+  child: Container(
+    margin: const Edgeeeeeee,
+  ),      
+      ),
+    );
   }
 }
