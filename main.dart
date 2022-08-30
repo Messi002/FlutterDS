bfecf4b91bbd325ae9baab50228ac38ace26aec0
Messi002 <rostandmessi2@gmail.com>
2022-08-30 12:36:07 +0100

    FlutterUIs

--- main.dart
+++ main.dart
@@ -1,5 +1,5 @@
 void main(List<String> args) {
-  Iterable<int> iterables = [1, 2, 3, 4];
+  Iterable<int> iterables = [1, 2, 3, 4, 6, 8];
   print(iterables.elementAt(1));
 
   bool test = iterables.every((element) => element < 3);
@@ -8,5 +8,9 @@ void main(List<String> args) {
   var evenNum = iterables.where((elt) => elt.isEven);
   for (var num in evenNum) {
     print('$num is even');
+  }
+  Iterable<int> output = iterables.map((e) => e * 2);
+  for (var l in output) {
+    print('$l has been multiplied by 2');
   }
 }
