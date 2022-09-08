cb077f5dfd82f399d9dd104f996333dcd5fad673
Messi002 <rostandmessi2@gmail.com>
2022-09-08 14:23:35 +0100

    FlutterUIs

--- NormalWidgetTesting/flutt_app1/lib/Screen1/image_fetching.dart
+++ NormalWidgetTesting/flutt_app1/lib/Screen1/image_fetching.dart
@@ -1,4 +1,10 @@
+import 'dart:convert';
+
+
 import 'package:flutter/material.dart';
+
+
+import 'package:http/http.dart' as http;
 
 class ImagePage extends StatefulWidget {
   const ImagePage({Key? key}) : super(key: key);
@@ -11,7 +17,8 @@ class _ImagePageState extends State<ImagePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      appBar: AppBar(title: const Text('Image Fetching'),),
+      body: ,
     );
   }
 }
