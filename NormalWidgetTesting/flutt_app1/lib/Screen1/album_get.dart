f614b4ba121b4a81988e1f9846e190ced402bcf6
Messi002 <rostandmessi2@gmail.com>
2022-09-10 20:12:30 +0100

    FlutterUIs

--- NormalWidgetTesting/flutt_app1/lib/Screen1/album_get.dart
+++ NormalWidgetTesting/flutt_app1/lib/Screen1/album_get.dart
@@ -1,6 +1,9 @@
-import 'package:flut_app/Components/appbar_widget.dart';
-import 'package:flutter/material.dart';
+import 'dart:convert';
 
+import 'package:flut_app/Components/appbar_widget.dart';
+import 'package:flut_app/Models/album_model.dart';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class AlbumPostPage extends StatefulWidget {
   const AlbumPostPage({Key? key}) : super(key: key);
@@ -11,6 +14,19 @@ class AlbumPostPage extends StatefulWidget {
 }
 
 class _AlbumPostPageState extends State<AlbumPostPage> {
+  final apiUrl = 'https://jsonplaceholder.typicode.com/albums/1';
+  Future<AlbumModel> fetchAlbum() async {
+    try {
+      http.Response response =
+          await http.get(Uri.parse(apiUrl)) as http.Response;
+      if (response.statusCode == 200) {
+        return AlbumModel.fromJson(jsonDecode(response.body));
+      } else {}
+    } catch (e) {
+      print(e);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
