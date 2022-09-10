bdd15222032bb425b9c60f29b2c5bf711e1c52e2
Messi002 <rostandmessi2@gmail.com>
2022-09-10 19:02:57 +0100

    FlutterUIs

--- NormalWidgetTesting/flutt_app1/lib/Screen1/album_get.dart
+++ NormalWidgetTesting/flutt_app1/lib/Screen1/album_get.dart
@@ -1,1 +1,15 @@
 import 'package:flutter/material.dart';
+
+class AlbumPost extends StatefulWidget {
+  const AlbumPost({Key? key}) : super(key: key);
+
+  @override
+  State<AlbumPost> createState() => _AlbumPostState();
+}
+
+class _AlbumPostState extends State<AlbumPost> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
