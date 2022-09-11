c64fb85e1aaef92a379c0debe6e61564c2f7928b
Messi002 <rostandmessi2@gmail.com>
2022-09-11 13:34:14 +0100

    FlutterUIs

--- todoey/lib/Screens/add_task_screen.dart
+++ todoey/lib/Screens/add_task_screen.dart
@@ -16,22 +16,35 @@ class _AddTaskScreenState extends State<AddTaskScreen> {
       color: Color(0xff757575),
       child: Container(
         decoration: BoxDecoration(
-          color: Colors.white,
-        borderRadius : BorderRadius.only(
-          topLeft: Radius.circular(20.0),
-          topRight: Radius.circular(20.0)
-        )
-        ),
-        child: Column(
-          // ignore: prefer_const_literals_to_create_immutables
-          children: <Widget>[
-            Text('Add Task', style:TextStyle(color: Colors.lightBlueAccent, fontSize: 14 ),),
-            SizedBox(height:8),
-            TextField(
-              autofocus:true;
-              
-            )
-          ],
+            color: Colors.white,
+            borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(20.0),
+                topRight: Radius.circular(20.0))),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 40.0, vertical: 35),
+          child: Column(
+            // ignore: prefer_const_literals_to_create_immutables
+            children: <Widget>[
+              Text(
+                'Add Task',
+                style: TextStyle(color: Colors.lightBlueAccent, fontSize: 22),
+              ),
+              SizedBox(height: 8),
+              TextField(
+                autofocus: true,
+              ),
+              SizedBox(height: 10),
+              RawMaterialButton(
+                onPressed: null,
+                constraints:
+                    BoxConstraints(minWidth: double.infinity, minHeight: 35),
+                fillColor: Colors.lightBlueAccent,
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(3)),
+                child: Text('Add', softWrap: true, style: TextStyle(color: Colors.white,),
+              ),
+            ],
+          ),
         ),
       ),
     );
