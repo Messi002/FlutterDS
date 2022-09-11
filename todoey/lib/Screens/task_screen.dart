a77004846f64f879eb420140f4e698066861694b
Messi002 <rostandmessi2@gmail.com>
2022-09-11 12:22:50 +0100

    FlutterUIs

--- todoey/lib/Screens/task_screen.dart
+++ todoey/lib/Screens/task_screen.dart
@@ -35,7 +35,10 @@ class TasksScreen extends StatelessWidget {
             Text('12 Tasks', style: TextStyle(
               color: Colors.white,
               fontSize: 18.0,
-            ),)
+            ),),
+            Container(
+              
+            ),
           ],
         ),
       ),
