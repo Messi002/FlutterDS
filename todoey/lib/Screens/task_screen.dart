1eee08fce6a65f745837672eb4b8977fde0e96a1
Messi002 <rostandmessi2@gmail.com>
2022-09-11 12:21:54 +0100

    FlutterUIs

--- todoey/lib/Screens/task_screen.dart
+++ todoey/lib/Screens/task_screen.dart
@@ -12,17 +12,29 @@ class TasksScreen extends StatelessWidget {
       body: Container(
         padding: EdgeInsets.only(top: 60.0, left: 30, right: 30, bottom: 30),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           // ignore: prefer_const_literals_to_create_immutables
           children: <Widget>[
             CircleAvatar(
                 backgroundColor: Colors.white,
                 radius: 30.0,
-                child: Icon(Icons.list, size: 30, color: Colors.black,)),
+                child: Icon(
+                  Icons.list,
+                  size: 30,
+                  color: Colors.black,
+                )),
             SizedBox(height: 10.0),
-            Text('Todoey', style: TextStyle(
-              fontWeight: FontWeight.w700,
-              fontSize:50.0,
+            Text(
+              'Todoey',
+              style: TextStyle(
+                fontWeight: FontWeight.w700,
+                fontSize: 50.0,
+                color: Colors.white,
+              ),
+            ),
+            Text('12 Tasks', style: TextStyle(
               color: Colors.white,
+              fontSize: 18.0,
             ),)
           ],
         ),
