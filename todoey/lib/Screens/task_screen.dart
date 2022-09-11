49a37407e68a8df915479c4dbfaf749a7e5eadec
Messi002 <rostandmessi2@gmail.com>
2022-09-11 12:18:57 +0100

    FlutterUIs

--- todoey/lib/Screens/task_screen.dart
+++ todoey/lib/Screens/task_screen.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
 
 class TasksScreen extends StatelessWidget {
@@ -8,11 +10,20 @@ class TasksScreen extends StatelessWidget {
     return Scaffold(
       backgroundColor: Colors.lightBlueAccent,
       body: Container(
-        padding:EdgeInsets.only(top:60.0,left:30,right:30, bottom:30),
+        padding: EdgeInsets.only(top: 60.0, left: 30, right: 30, bottom: 30),
         child: Column(
+          // ignore: prefer_const_literals_to_create_immutables
           children: <Widget>[
-            Icon(Icons.list),
-            Text('Todoey')
+            CircleAvatar(
+                backgroundColor: Colors.white,
+                radius: 30.0,
+                child: Icon(Icons.list, size: 30, color: Colors.black,)),
+            SizedBox(height: 10.0),
+            Text('Todoey', style: TextStyle(
+              fontWeight: FontWeight.w700,
+              fontSize:50.0,
+              color: Colors.white,
+            ),)
           ],
         ),
       ),
