4addf166536cb0d4dd7a44284818813e6f283e0e
Messi002 <rostandmessi2@gmail.com>
2022-09-12 13:25:13 +0100

    FlutterUIs

--- todoey/lib/widgets/task_list.dart
+++ todoey/lib/widgets/task_list.dart
@@ -5,7 +5,11 @@ import 'package:todoey/widgets/task_tile.dart';
 import '../Model/task_model.dart';
 
 class TaskList extends StatelessWidget {
-  List<Task> task = [];
+  List<Task> task = [
+    Task(name: 'buy milk'),
+    Task(name: 'buy bread'),
+    Task(name: 'buy eggs'),
+  ];
   @override
   Widget build(BuildContext context) {
     return ListView(
