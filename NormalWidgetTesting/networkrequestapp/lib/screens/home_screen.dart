e81bcd757580f188a8521a82d8496185822fd223
Messi002 <rostandmessi2@gmail.com>
2022-09-12 15:31:35 +0100

    FlutterUIs

--- NormalWidgetTesting/networkrequestapp/lib/screens/home_screen.dart
+++ NormalWidgetTesting/networkrequestapp/lib/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flut_app/controller/todo_controller.dart';
+import 'package:flut_app/models/todo.dart';
 import 'package:flut_app/repository/todo_repository.dart';
 import 'package:flutter/material.dart';
 
@@ -12,14 +13,33 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Rest API')),
-      body: Container(
-        child: Center(
-          child: ElevatedButton(
-              onPressed: () {
-                todoController.fetchTodoList();
-              },
-              child: Text('Fetch')),
-        ),
+      body: FutureBuilder<List<Todo>>(
+        future: todoController.fetchTodoList(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+
+          if (snapshot.hasError) {
+            return Center(
+              child: Text('${snapshot.error}'),
+            );
+          }
+          return ListView.separated(
+            itemBuilder: (context, index) {
+              return Container();
+            },
+            separatorBuilder: (context, index) {
+              return Divider(
+                thickness: 0.5,
+                height: 0.5,
+              );
+            },
+            itemCount: snapshot.data?.length ?? 0,
+          );
+        },
       ),
     );
   }
