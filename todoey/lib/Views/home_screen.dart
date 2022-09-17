d4f7258684dd8f35422db5d253fb5608a81fbb49
Messi002 <rostandmessi2@gmail.com>
2022-09-17 18:00:49 +0100

    FlutterUIs

--- todoey/lib/Views/home_screen.dart
+++ todoey/lib/Views/home_screen.dart
@@ -5,9 +5,9 @@ import 'package:todoey/Screens/task_screen.dart';
 import 'package:flutter/foundation.dart';
 
 class HomeScreen extends StatelessWidget {
- HomeScreen({super.key});
+  HomeScreen({super.key});
 
-  List<Color> colors = [Colors.red, Colors.blue,Colors.purple,Colors.teal];
+  List<Color> colors = [Colors.red, Colors.blue, Colors.purple, Colors.teal];
 
   @override
   Widget build(BuildContext context) {
@@ -15,56 +15,70 @@ class HomeScreen extends StatelessWidget {
     final locale = Localizations.localeOf(context);
     return Scaffold(
       appBar: AppBar(title: Text('Welcome')),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Center(
-              child: Text('devicePixelRatio: ${mediaQuery.devicePixelRatio}')),
-          Center(child: Text('height: ${mediaQuery.size.height}')),
-          Center(child: Text('width: ${mediaQuery.size.width}')),
-          Center(child: Text('width: $locale')),
-          Center(
-              child: Text(
-                  'platfomr: ${defaultTargetPlatform == TargetPlatform.linux}')),
-          Center(child: Text('Again: $defaultTargetPlatform')),
-          Semantics(
-            button: true,
-            label: 'This is just a semantic test button',
-            child: ElevatedButton(
-                onPressed: () {
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          fullscreenDialog: true,
-                          builder: (context) => TasksScreen()));
-                },
-                child: Text('Hello')),
-          ),
-          LayoutBuilder(builder: (context, constraints) {
-            if (constraints.maxHeight < 500 || constraints.maxWidth > 500) {
-              return Row(
-                children: List.generate(4, (index) {
-                  return Expanded(
-                      child: Container(
-                    color: colors[index],
-                    child: Text('$index'),
-                  ));
-                }),
-              );
-            }
-
-            return Column(
+      body: LayoutBuilder(builder: (context, constraints) {
+        if (constraints.maxHeight < 500 || constraints.maxWidth > 500) {
+          return SafeArea(
+            left: false,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
               children: List.generate(4, (index) {
                 return Expanded(
                     child: Container(
                   color: colors[index],
-                  child: Text('$index'),
+                  child: Center(
+                    child: Text(
+                      '$index',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(fontSize: 25),
+                    ),
+                  ),
                 ));
               }),
-            );
+            ),
+          );
+        }
+
+        return Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: List.generate(4, (index) {
+            return Expanded(
+                child: Container(
+              color: colors[index],
+              child: Center(
+                child: Text(
+                  '$index',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(fontSize: 25),
+                ),
+              ),
+            ));
           }),
-        ],
-      ),
+        );
+      }),
     );
   }
 }
+
+
+// Center(
+          //     child: Text('devicePixelRatio: ${mediaQuery.devicePixelRatio}')),
+          // Center(child: Text('height: ${mediaQuery.size.height}')),
+          // Center(child: Text('width: ${mediaQuery.size.width}')),
+          // Center(child: Text('width: $locale')),
+          // Center(
+          //     child: Text(
+          //         'platfomr: ${defaultTargetPlatform == TargetPlatform.linux}')),
+          // Center(child: Text('Again: $defaultTargetPlatform')),
+          // Semantics(
+          //   button: true,
+          //   label: 'This is just a semantic test button',
+          //   child: ElevatedButton(
+          //       onPressed: () {
+          //         Navigator.push(
+          //             context,
+          //             MaterialPageRoute(
+          //                 fullscreenDialog: true,
+          //                 builder: (context) => TasksScreen()));
+          //       },
+          //       child: Text('Hello')),
+          // ),
